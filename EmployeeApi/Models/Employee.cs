282799bc165ad75c534825ff3a9aa0170bc09873
gilbertkibet <kibetgilly354@gmail.com>
2022-12-12 10:25:46 +0300

    some test

--- EmployeeApi/Models/Employee.cs
+++ EmployeeApi/Models/Employee.cs
@@ -9,6 +9,7 @@ namespace EmployeeApi.Models
         public string Email { get; set; }
 
         public long Phone { get;set; }
+
         public long Salary { get; set; }
 
         public string Department { get; set; }
