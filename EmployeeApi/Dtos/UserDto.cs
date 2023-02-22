a40b4522c3dc1a38734f67b73c10d5d061710929
Gilbert kibet <kibetgilly354@gmail.com>
2023-02-22 09:11:50 -0800

    Generated a token

---
+++ EmployeeApi/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace EmployeeApi.Dtos
+{
+    public class UserDto
+    {
+        public  string UserName { get; set; }   
+        
+        public string Password { get; set; }
+
+    }
+}
---
+++ EmployeeApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeApi.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }=string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+    }
+}
